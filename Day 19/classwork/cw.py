5398046ac37a8302e7813e6c3f1544353c346bfc
Temomakhatadze <temurikatemo2011@gmail.com>
2025-02-27 18:02:57 +0400

    commit

---
+++ Day 19/classwork/cw.py
@@ -0,0 +1,24 @@
+#1
+for x in range (150,203,2 ) :
+    print(x) #
+
+#2
+for i in range(400,199,-1) :
+    print(i)
+#3
+ #while-sanam cikli mushaobs  da magisi daregulireba ar shegvidzlia
+ #for- roca vici cikli da magissi daregurileba sheidzleba
+
+ #4 
+total=10
+while total >1:
+    print(total)
+    total +=1
+
+    #5
+    total=1
+while total <21 : 
+    print(total)
+    total +=1
+
+    
---
+++ Day 19/homework/hw.py
@@ -0,0 +1,25 @@
+#1
+ #while-sanam cikli mushaobs  da magisi daregulireba ar shegvidzlia
+ ## Loop using i as internal variable
+#2
+total=1
+while total <100:
+    print("goa the best")
+    total +=1
+#3
+total=-10
+while total <0:
+    print(total)
+    total +=1
+#4
+total=1
+while total <50:
+    print(total)
+    total +=1
+#5
+
+for i in range(4):
+  print("ნიკა, თეო, ანა, გიო")
+#6
+for i in range(7):
+  print("მე მიყვარს პროგრამირება")
