f036d9a1ea68e207362c88559dbd1c9fed1e33fb
Temomakhatadze <temurikatemo2011@gmail.com>
2025-03-03 15:26:22 +0400

    commit
    goa

---
+++ Day 21/classwork/cw.py
@@ -0,0 +1,15 @@
+#1
+car1="mersedes"
+car2="bmw"
+car3="mclaren"
+print(car1+" "+car2+" "+car3)
+#2
+food1="xinkali"
+food2="xhatchapuri"
+food3="imeruli yveli"
+print(food1+" "+food2+" "+food3)
+#3
+num = [1,  2,  3,   4,  5,  6,  7,   8,  9,  10]
+print(num)
+print(num[1 : 8])
+print(num[ 4 : 10])
---
+++ Day 21/homework/hw.py
@@ -0,0 +1,16 @@
+#2
+num = [95,  5,  4,   7,  1,  9,  12,   654,  1,  10]
+for i in num:
+   print(i)
+#2
+num = [1,2,3,4,5,6,7,8, 9, 10]
+print(num)
+print(num[0:5])
+#4
+num = [1,2,3,4,5,6,7,8, 9, 10]
+print(num)
+print(num[3:7])
+#5
+num2 =[200,400,550,4231,432]
+for i in num2:
+    print(i)
