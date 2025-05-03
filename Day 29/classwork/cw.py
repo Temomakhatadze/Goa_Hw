2e8f4c956643475c3517023af3635526fe082adb
Temomakhatadze <temurikatemo2011@gmail.com>
2025-05-03 15:54:44 +0400

    commit
    helloooo

---
+++ Day 29/classwork/cw.py
@@ -0,0 +1,13 @@
+#1
+def name_to_upper(name):
+    result = ""
+    for  name in name:
+        result += name.upper()
+    return result
+print(name_to_upper("temo"))
+#2
+def print_list_elements(my_list):
+    for element in my_list:
+        print(element)
+ხილიიი = ['ვაშლი', 'მსხალი', 'ბანანი']
+print_list_elements(ხილიიი)
